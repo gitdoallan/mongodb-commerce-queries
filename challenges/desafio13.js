34958e5e329268780feebee26a603e7b0d8c4241
gitdoallan <mailtoallan@icloud.com>
2022-09-15 11:10:37 -0300

    feat: req 11 to 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,6 @@
+// Inclua o campo criadoPor em todos os documentos, colocando Ronald McDonald no valor desse campo
+
+db.produtos.updateMany(
+  {},
+  { $set: { criadoPor: "Ronald McDonald" } },
+);
