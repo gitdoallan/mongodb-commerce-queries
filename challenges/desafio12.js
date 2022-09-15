8d8f4c18eefc17b5fd4880598c7d8ab0a8b26dd9
gitdoallan <mailtoallan@icloud.com>
2022-09-15 11:14:05 -0300

    fix: missing returned values req 12 and 13

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -4,3 +4,10 @@ db.produtos.updateMany(
   { nome: { $ne: "McChicken" } },
   { $addToSet: { ingredientes: "ketchup" } },
 );
+
+// Retorne o nome e ingredientes de todos os documentos
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, ingredientes: 1 },
+);
--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -4,3 +4,10 @@ db.produtos.updateMany(
   {},
   { $set: { criadoPor: "Ronald McDonald" } },
 );
+
+// Retorne o nome e criadoPor de todos os documentos
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, criadoPor: 1 },
+);
