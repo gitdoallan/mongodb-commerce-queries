d403d986e387a4f02641fdf807afcfd09a564ac7
gitdoallan <mailtoallan@icloud.com>
2022-09-15 11:45:52 -0300

    fix: lint

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,4 +1,4 @@
-//Retorne o nome e vendidos dos lanches que tenham sido vendidos com uma quantidade diferente de 50 e em que o campo tags não exista
+// Retorne o nome e vendidos dos lanches que tenham sido vendidos com uma quantidade diferente de 50 e em que o campo tags não exista
 
 db.produtos.find(
   { vendidos: { $ne: 50 }, tags: { $exists: false } },
